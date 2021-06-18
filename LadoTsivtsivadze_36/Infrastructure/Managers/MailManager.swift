6c6ba4ab745b7ac36f262da7dfb239e893ab9550
lado at luka <tsivtsivadzelado@yahoo.com>
2021-06-18 04:37:16 -0700

    started implementing managers

---
+++ LadoTsivtsivadze_36/Infrastructure/Managers/MailManager.swift
@@ -0,0 +1,10 @@
+//
+//  MailManager.swift
+//  LadoTsivtsivadze_36
+//
+//  Created by lado tsivtsivadze on 6/18/21.
+//
+
+import Foundation
+import UIKit
+import CoreData
---
+++ LadoTsivtsivadze_36/Infrastructure/Managers/UserManager.swift
@@ -0,0 +1,55 @@
+//
+//  UserManager.swift
+//  LadoTsivtsivadze_36
+//
+//  Created by lado tsivtsivadze on 6/18/21.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+
+final class UserManager {
+    var context: NSManagedObjectContext? {
+        return (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    }
+    
+    func register(username name: String, password pass: String, completion: @escaping (Bool) -> Void) {
+        guard let context = context else { return }
+        
+        let user = User(context: context)
+        user.username = name
+        user.password = pass
+        
+        do {
+            try context.save()
+            completion(true)
+        }
+        catch {
+            print(error)
+            completion(false)
+        }
+    }
+    
+    func login(username name: String, password pass: String, completion: @escaping (User?) -> Void) {
+        guard let context = context else { return }
+        
+        let request = NSFetchRequest<User>(entityName: "User")
+        request.predicate = NSPredicate(format: "username = '\(name)' && password = '\(pass)'")
+        
+        do {
+            let user = try context.fetch(request)
+            if let user = user.last {
+                completion(user)
+            }
+            else {
+                completion(nil)
+            }
+        }
+        catch {
+            print(error)
+            completion(nil)
+        }
+    }
+}

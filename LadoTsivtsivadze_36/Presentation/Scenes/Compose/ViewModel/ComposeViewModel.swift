45c4c12ee12f1802bb6931c4ec6463b0f07a8c83
lado at luka <tsivtsivadzelado@yahoo.com>
2021-06-18 22:26:18 -0700

    creating pickerview

--- LadoTsivtsivadze_36/Presentation/Scenes/Compose/ViewModel/ComposeViewModel.swift
+++ LadoTsivtsivadze_36/Presentation/Scenes/Compose/ViewModel/ComposeViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 
-protocol ComposeViewModelProtocol: NSObject, UIPickerViewDataSource {
+protocol ComposeViewModelProtocol: NSObject, UIPickerViewDataSource, UIPickerViewDelegate {
     func sendMail(from user1: User,
                   to user2: User,
                   subject sbjct: String,
@@ -30,16 +30,20 @@ final class ComposeViewModel: NSObject, ComposeViewModelProtocol {
     private var pickerView: UIPickerView!
     
     var usersExceptMe: [User] = []
+    var me: User?
 
     init(with1 mailManager2: MailManager,
          with2 userManager2: UserManager,
          with3 controller: ComposeController,
          with4 pickerView2: UIPickerView) {
         
+        super.init()
         mailManager = mailManager2
         userManager = userManager2
         rootController = controller
         pickerView = pickerView2
+        
+        getOtherUsers()
     }
     
     func sendMail(from user1: User,
@@ -51,17 +55,29 @@ final class ComposeViewModel: NSObject, ComposeViewModelProtocol {
         mailManager.createMail(from: user1, to: user2, subject: sbjct, content: cnt, completion: completion)
     }
     
-    func getOtherUsers(except user: User, completion: @escaping ([User]?) -> Void) {
-        userManager.getAllUsersExceptMe(except: user, completion: completion)
+    func getOtherUsers() {
+        guard let me = me else { return }
+        userManager.getAllUsersExceptMe(except: me) { users in
+            if let users = users {
+                self.usersExceptMe.append(contentsOf: users)
+            }
+            else {
+                print("Can not get users except me")
+            }
+        }
     }
 }
 
-extension ComposeViewModel: UIPickerViewDataSource {
+extension ComposeViewModel: UIPickerViewDataSource, UIPickerViewDelegate {
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         1
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         usersExceptMe.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        usersExceptMe[row].username
     }
 }
